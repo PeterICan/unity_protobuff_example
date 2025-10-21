cccc09492bc6cb60d9a7d4eb6c25bce725e3e4a9
PeterICan <peter.chen@icantw.com>
2025-10-21 12:21:29 +0800

    chore: [通用] 封包結構調整

--- common/generate_csharp_proto.bat
+++ common/generate_csharp_proto.bat
@@ -1,7 +1,9 @@
 @echo off
 rem This script generates C# code from the .proto file.
 
-set PROTO_DIR=.\proto
+pushd %~dp0
+
+set PROTO_ROOT_DIR=.\proto
 set CSHARP_OUT_DIR=..\client\Assets\Generated
 
 echo Ensuring C# output directory exists: %CSHARP_OUT_DIR%
@@ -9,7 +11,12 @@ if not exist "%CSHARP_OUT_DIR%" (
     mkdir "%CSHARP_OUT_DIR%"
 )
 
-echo Generating C# code...
-protoc --proto_path=%PROTO_DIR% --csharp_out=%CSHARP_OUT_DIR% message.proto
+echo Generating C# code for message.proto...
+protoc --proto_path=%PROTO_ROOT_DIR% --csharp_out=%CSHARP_OUT_DIR% message.proto
+
+echo Generating C# code for json_api protos...
+protoc --proto_path=%PROTO_ROOT_DIR% --csharp_out=%CSHARP_OUT_DIR% %PROTO_ROOT_DIR%\json_api\*.proto
 
 echo C# code generation complete.
+
+popd
--- common/generate_go_proto.bat
+++ common/generate_go_proto.bat
@@ -1,7 +1,9 @@
 @echo off
 rem This script generates Go code from the .proto file.
 
-set PROTO_DIR=.\proto
+pushd %~dp0
+
+set PROTO_ROOT_DIR=.\proto
 set GO_OUT_DIR=..\server\generated
 
 echo Ensuring Go output directory exists: %GO_OUT_DIR%
@@ -9,7 +11,12 @@ if not exist "%GO_OUT_DIR%" (
     mkdir "%GO_OUT_DIR%"
 )
 
-echo Generating Go code...
-protoc --proto_path=%PROTO_DIR% --go_out=%GO_OUT_DIR% message.proto
+echo Generating Go code for message.proto...
+protoc --proto_path=%PROTO_ROOT_DIR% --go_out=paths=source_relative:%GO_OUT_DIR% message.proto
+
+echo Generating Go code for json_api protos...
+protoc --proto_path=%PROTO_ROOT_DIR% --go_out=paths=source_relative:%GO_OUT_DIR% %PROTO_ROOT_DIR%\json_api\*.proto
 
 echo Go code generation complete.
+
+popd
